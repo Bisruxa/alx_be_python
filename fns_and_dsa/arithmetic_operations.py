c7ad8f55a9914d18117ce7d57b8ccb1c909e94e0
bisruxa <imbisru@gmail.com>
2024-09-25 11:06:19 -0900

    arthimetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation(num1,num2,operation):
+  result = ""
+  match operation:
+    case 'add':
+      result = num1 + num2
+    case 'subtract':
+      result = num1 - num2
+    case 'multiply':
+      result = num1 * num2
+    case 'divide':
+      if(num2 == 0):
+        print("division by zero is undefined")
+      else:
+        result = num1/num2
+  return result
+
+
