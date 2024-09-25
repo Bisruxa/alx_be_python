3e2227b77d35cea65d68b9f76417a294fb8d1f8d
bisruxa <imbisru@gmail.com>
2024-09-25 11:11:58 -0900

    c

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,7 +10,7 @@ def perform_operation(num1, num2, operation):
     case 'divide':
       if(num2 == 0):
         print("division by zero is undefined")
-      else:
+      elif(num2 != 0):
         result = num1/num2
   return result
 
