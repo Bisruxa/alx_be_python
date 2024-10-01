96d65512070d2789331fcb7b3ad589415299cfaf
bisruxa <imbisru@gmail.com>
2024-10-01 01:37:14 -0900

    to be changed

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,16 +1,45 @@
-from datetime import datetime,timedelta
+# from datetime import datetime,timedelta
+# def display_current_datetime():
+#   current_date = datetime.now()
+#   formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+#   print(f"Current date and time: {formatted_date}")
+
+
+# def calculate_future_date(days):
+#  current_date = datetime.now()
+#  future_date = current_date + timedelta(days=days)
+#  formatted_future_date = future_date.strftime("%Y-%m-%d")
+#  print(f"Future date: {formatted_future_date}")
+#  return future_date
+# display_current_datetime()
+# number_of_days= int(input("Enter the number of days to add to the current date: "))
+# calculate_future_date(number_of_days)
+from datetime import datetime, timedelta
+
 def display_current_datetime():
-  current_date = datetime.now()
-  formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
-  print(f"Current date and time: {formatted_date}")
+    current_date = datetime.now()
 
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
 
-def calculate_future_date(days):
- current_date = datetime.now()
- future_date = current_date + timedelta(days=days)
- formatted_future_date = future_date.strftime("%Y-%m-%d")
- print(f"Future date: {formatted_future_date}")
- return future_date
-display_current_datetime()
-number_of_days= int(input("Enter the number of days to add to the current date: "))
-calculate_future_date(number_of_days)
+    print(f"Current date and time: {formatted_date}")
+
+def calculate_future_date(days_to_add):
+    current_date = datetime.now().date()
+    
+
+    future_date = current_date + timedelta(days=days_to_add)
+
+    print(f"Future date: {future_date}")
+
+def main():
+    display_current_datetime()
+
+    try:
+        days_to_add = int(input("Enter the number of days to add to add to the current date: "))
+
+        calculate_future_date(days_to_add)
+    except ValueError:
+        print("Please enter a valid integer.")
+
+if __name__ == "__main__":
+    main()
