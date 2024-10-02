2fe09df80fd6f60695307c10d21f439671ee0c2b
bisruxa <imbisru@gmail.com>
2024-10-02 11:21:08 -0900

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,25 @@
+class BankAccount:
+  def __init__(self,initial_balance=0):
+   self.account_balance = initial_balance
+   
+  def deposit(self,amount):
+   if amount >0 :
+     self.account_balance +=amount
+     print(f"Depposited: ${amount}")
+   else:
+     print("deposite amount must be positive")
+
+   
+  def withdraw(self,amount):
+    if amount>0 and amount <= self.account_balance:
+     self.account_balance -= amount
+     print(f"Withdrae: ${amount}")
+     return True
+    elif amount > self.account_balance:
+     print("Insufficient funds")
+     return False
+    else:
+      print("amount must be positive")
+      return False
+  def display_balance(self):
+    print(f"your current balance is {self.account_balance}")
