0cb15d006e85bc196bf71ebf59320c7d94ca8317
bisruxa <imbisru@gmail.com>
2024-10-04 04:24:42 -0900

    commited

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -3,12 +3,20 @@ class Book:
     self.title = title
     self.author = author
     self._is_checked_out = is_checked_out
+  def CheckOut(self):
+    pass
+  def ReturnBook(self):
+    pass
 class Library(Book):
-  def __init__(self,books):
-    self._books = books
-  def add_book(self):
-   self.title = Book.title
-   self.author = Book.author
+  def __init__(self,title,author,books=None):
+    super.__init__(self,title,author)
+    if books is None:
+     self._books = []
+    else:
+      self._books = books
+  def add_book(self,books):
+   if books.title not in self.title:
+     self._books.append(books)
   def check_out_title(self,title):
     self.title =title
   def return_book(self,title):
