7966310bf67368707ded84acc2d422a5c9e2828b
bisruxa <imbisru@gmail.com>
2024-10-04 03:30:04 -0900

    succefully tested

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,26 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class testSimpleCalculator(unittest.TestCase):
+  def setUp(self):
+    self.calc = SimpleCalculator()
+  def test_add(self):
+    self.assertEqual(self.calc.add(4,5) , 9)
+    self.assertEqual(self.calc.add(-1,1) , 0)
+    self.assertEqual(self.calc.add(-1,-1) , -2)
+  def test_subtract(self):
+    self.assertEqual(self.calc.subtract(5,3) , 2)
+    self.assertEqual(self.calc.subtract(3,5) , -2)
+    self.assertEqual(self.calc.subtract(-3,3) , -6)
+    self.assertEqual(self.calc.subtract(-3,-3) , 0)
+
+  def test_multiply(self):
+    self.assertEqual(self.calc.multiply(4,5) , 20)
+    self.assertEqual(self.calc.multiply(-4,5) , -20)
+    self.assertEqual(self.calc.multiply(-4,-5) , 20)
+
+
+  def test_divide(self):
+    self.assertEqual(self.calc.divide(4,0), None)
+    self.assertEqual(self.calc.divide(4,2) , 2)
+
