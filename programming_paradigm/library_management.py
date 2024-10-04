f97e9a116ddddbb81d6f6c3cf415d01bf7a0f40e
bisruxa <imbisru@gmail.com>
2024-10-04 09:03:12 -0900

    check out method fixed

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,22 +1,25 @@
 class Book:
-  def __init__(self,title,author,is_checked_out):
+  def __init__(self,title,author):
     self.title = title
     self.author = author
-    self._is_checked_out = is_checked_out
+    self._is_checked_out = False
   def CheckOut(self):
-    pass
+# this method returns false if the book is already checked outindicating that the checkout operation was no successful
+    if self._is_checked_out:
+      return False
+    self._is_checked_out = True
+    return True
   def ReturnBook(self):
-    pass
-class Library(Book):
-  def __init__(self,title,author,books=None):
-    super.__init__(self,title,author)
-    if books is None:
-     self._books = []
-    else:
-      self._books = books
-  def add_book(self,books):
-   if books.title not in self.title:
-     self._books.append(books)
+    if not self._is_checked_out:
+      return False
+    self._is_checked_out = False
+    return True
+class Library:
+  def __init__(self):
+    self._books = []
+  def add_book(self,book):
+   if book.title not in self.title:
+     self._books.append(book)
   def check_out_title(self,title):
     self.title =title
   def return_book(self,title):
