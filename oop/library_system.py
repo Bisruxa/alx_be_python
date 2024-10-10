762e2f5d53566adfb9978a8bc3ff407fdda9c0d0
bisruxa <imbisru@gmail.com>
2024-10-10 07:01:22 -0900

    spacing have been added

--- oop/library_system.py
+++ oop/library_system.py
@@ -8,15 +8,13 @@ class EBook(Book):
  def __init__(self,title,author,file_size):
   super().__init__(title,author)
   self.file_size = file_size
- 
-
-class PrintBook(Book):
+ class PrintBook(Book):
   def __init__(self,title,author,page_count):
    super().__init__(title,author)
    self.page_count = page_count
 class Library:
  def __init__(self):
-  self.books=[]
+  self.books = []
  def add_books(self,book):
   self.books.append(book)
  def list_books(self):
